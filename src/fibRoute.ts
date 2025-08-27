a91e9abb6363e80a3b5585fc5d1e6f76dd5aab41
wi0n <lhhsandyp@gmail.com>
2025-08-26 23:16:15 -0400

    Added type imports and proper param/return type translation

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,11 +1,12 @@
 // Endpoint for querying the fibonacci numbers
 
-import fibonacci from "./fib";
+import { Request, Response } from 'express';
+import fibonacci from './fib';
 
-export default (req, res) => {
+export default (req: Request, res: Response): void => {
   const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
+  const fibN = fibonacci(parseInt(num, 10));
   let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
